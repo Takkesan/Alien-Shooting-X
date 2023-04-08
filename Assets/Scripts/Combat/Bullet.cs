bc5f230d6749cf7f5ff9a580284569ec701f2e13
Takeru <ktake393@gmail.com>
2023-04-08 21:54:44 +0900

    弾が前進するようにした

---
+++ Assets/Scripts/Combat/Bullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float speed_ = 5.0f;
+
+    
+    void Update()
+    { 
+        transform.Translate(Vector3.right * speed_ * Time.deltaTime);
+    }
+}
